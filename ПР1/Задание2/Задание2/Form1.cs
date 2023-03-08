58ad81c19abd40034e937f3ad83de717e6abf6bf
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-03-08 21:08:55 +0300

    132

---
+++ ПР1/Задание2/Задание2/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание2
+{
+    public partial class Form1 : Form
+    {
+        decimal guess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnlyValidDecimal(in TextBox textbox, ref KeyPressEventArgs e)
+        {
+            char pressed = e.KeyChar;
+            // 8 - это BackSlash
+            //MessageBox.Show($"{textBox1.Text.Contains('1')}");
+            if (!Char.IsDigit(pressed) && !((int)pressed == 8))
+            {
+                if ((pressed != '.') && (pressed != ','))
+                {
+                    e.Handled = true;
+                }
+                if (textBox1.Text.Contains('.') || textBox1.Text.Contains(',')) e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            OnlyValidDecimal(in textBox1,ref e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string entered_value = textBox1.Text.ToString();
+            double parsed_value;
+            string error_message;
+            if (!parse_double_value(in entered_value, out parsed_value, out error_message))
+            {
+                MessageBox.Show(error_message);
+                return;
+            }
+            label1.Text = $"{Math.Sqrt(parsed_value)}";
+        }
+
+        private bool parse_double_value(in string to_parse, out double parsed, out string error_message)
+        {
+            error_message = "";
+            if (!double.TryParse("0" + to_parse.Replace('.', ','), out parsed))
+            {
+                error_message = "Пожалуйста, введите дробное число";
+                return false;
+            }
+            if (parsed <= 0.0)
+            {
+                error_message = "Пожалуйста, введите положительное число, не равное нулю";
+                return false;
+            }
+            return true;
+        }
+
+        private bool parse_decimal_value(in string to_parse, out decimal parsed, out string error_message)
+        {
+            double parsed_double;
+            parsed = 0;
+            if (!parse_double_value(in to_parse, out parsed_double, out error_message)) {
+                return false;
+            }
+            try
+            {
+                parsed = (decimal)parsed_double;
+            }
+            catch (System.OverflowException)
+            {
+                error_message = "Слишком большое число";
+                return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string entered_value = textBox1.Text.ToString();
+            decimal number_decimal;
+            string error_message;
+            if (!parse_decimal_value(in entered_value, out number_decimal, out error_message))
+            {
+                MessageBox.Show(error_message);
+                return;
+            }
+            decimal delta = Convert.ToDecimal(Math.Pow(10, -28));
+            this.guess = number_decimal / 2;
+            decimal result = ((number_decimal / this.guess) + this.guess) / 2;
+            while (Math.Abs(result - this.guess) > delta)
+            {
+                do_newton_iter(in number_decimal, ref result, ref this.guess);
+            }
+            label2.Text = $"{result}";
+        }
+        private void do_newton_iter(in decimal number, ref decimal result, ref decimal guess)
+        {
+            guess = result;
+            result = ((number / guess) + guess) / 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string entered_value = textBox1.Text.ToString();
+            decimal number_decimal;
+            string error_message;
+            if (!parse_decimal_value(in entered_value, out number_decimal, out error_message))
+            {
+                MessageBox.Show(error_message);
+                return;
+            }
+            decimal result;
+            entered_value = label2.Text.ToString();
+            if (!parse_decimal_value(entered_value, out result, out error_message))
+            {
+                result = number_decimal / 2;
+            }
+            do_newton_iter(in number_decimal, ref result, ref this.guess);
+            label2.Text = $"{result}";
+        }
+    }
+}

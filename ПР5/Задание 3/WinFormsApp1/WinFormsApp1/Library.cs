608415db7ad6630aea13fc37d9dc430c256ff204
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-06-01 13:41:13 +0300

    Исправлена ошибка

--- ПР5/Задание 3/WinFormsApp1/WinFormsApp1/Library.cs
+++ ПР5/Задание 3/WinFormsApp1/WinFormsApp1/Library.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace WinFormsApp1
 {
-    public abstract class Library : ISearch<Page>
+    public abstract class Library : ISearch<Book>
     {
-        public abstract Page Find(Func<Page, bool> predicate);
-        public abstract Page[] SearchAll();
+        public abstract Book Find(Func<Book, bool> predicate);
+        public abstract Book[] SearchAll();
     }
 }
